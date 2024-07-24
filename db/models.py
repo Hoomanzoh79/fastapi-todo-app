30eba461dde21bc977b90dea6d1b7b70b2f9e60e
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-07-24 11:13:27 +0330

    change id type to UUID

--- db/models.py
+++ db/models.py
@@ -1,3 +1,4 @@
+from uuid import UUID,uuid4
 from sqlalchemy.orm import Mapped,mapped_column
 
 from .engine import Base
@@ -5,9 +6,7 @@ from .engine import Base
 
 class User(Base):
     __tablename__ = "users"
-
-    id : Mapped[int] = mapped_column(primary_key=True)
     password: Mapped[str] = mapped_column()
-    username : Mapped[str] = mapped_column(
-        unique=True,nullable=True
-    )
+    username : Mapped[str] = mapped_column(unique=True)
+
+    id : Mapped[UUID] = mapped_column(primary_key=True,default_factory=uuid4)
