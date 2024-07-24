d0a20525e2d940ebce846509acca206ff8a7769e
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-07-24 09:52:42 +0330

    add async connection for db

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
+import asyncio
 
 from routers.users import router as user_router
+from db.engine import engine,Base
 
-app = FastAPI()
+
+async def init_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Run at startup
+    asyncio.create_task(init_tables())
+    yield
+    # Run on shutdown (if required)
+    print('Shutting down...')
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(user_router,prefix="/users")
