9f4e82bce925db0f5816aee077fbd5e090f6d4d6
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-07-24 11:50:02 +0330

    add register

--- routers/users.py
+++ routers/users.py
@@ -1,12 +1,19 @@
-from fastapi import APIRouter,Body
+from fastapi import APIRouter,Body, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+from typing import Annotated
 
 from schema._input import RegisterInput
+from db.engine  import get_db
+from operations.users import UserOperation
 
 router = APIRouter()
 
 @router.post("/register")
-async def register(data: RegisterInput = Body()):
-    return data
+async def register(db_session:Annotated[AsyncSession, Depends(get_db)],
+                   data: RegisterInput = Body(),
+):
+    user = await UserOperation(db_session).create(username=data.username,password=data.password)
+    return user
 
 @router.post("/login")
 async def login():
