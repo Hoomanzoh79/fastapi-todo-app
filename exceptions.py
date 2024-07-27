e2c08a7fd9686d8f75137b4758284b5a3820a381
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-07-27 12:04:09 +0330

    add UserAlreadyExistsException

--- exceptions.py
+++ exceptions.py
@@ -4,4 +4,9 @@ from fastapi import HTTPException,status
 class UserNotFoundException(HTTPException):
     def __init__(self):
         self.status_code = status.HTTP_404_NOT_FOUND
-        self.detail = "User not found! "
+        self.detail = "User not found!"
+
+class UserAlreadyExistsException(HTTPException):
+    def __init__(self):
+        self.status_code = status.HTTP_400_BAD_REQUEST
+        self.detail = "User already exists with the given username!"
