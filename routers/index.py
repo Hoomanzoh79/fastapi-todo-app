2522659c7738d0ff188ab5db1dda1e1191857673
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-08-05 18:36:26 +0330

    add add_task

--- routers/index.py
+++ routers/index.py
@@ -1,12 +1,14 @@
 from typing import Annotated
-from fastapi import Depends, Request,APIRouter
-from fastapi.responses import HTMLResponse
+from fastapi import Depends, Request,APIRouter,Form
+from fastapi.responses import HTMLResponse,RedirectResponse
 from fastapi.templating import Jinja2Templates
 import sqlalchemy as sa
 from sqlalchemy.orm import Session
+from fastapi import status
 
 from db.engine import get_db
 from db.models import Task
+from operations.tasks import TaskOperation
 
 class TemplateOperation:
     def __init__(self,db_session: Session) -> None:
@@ -30,3 +32,10 @@ templates = Jinja2Templates(directory="templates")
 async def index(db_session:db_dependency,request: Request):
     all_tasks = TemplateOperation(db_session).get_all_tasks()
     return templates.TemplateResponse("index.html", {"request": request, "all_tasks": all_tasks})
+
+
+@router.post("/create")
+async def add_task(db_session:db_dependency,request: Request, task_name: str = Form(...)):
+    task = TaskOperation(db_session).create(name=task_name,user_id=12) # will get the user from request.user later
+    redirect_url = request.url_for('index')
+    return RedirectResponse(redirect_url)
